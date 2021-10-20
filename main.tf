f2550aa3abe696022f31c23e6f00a728a94a4eb3
Adedayo Akinpelu <adedayoakinpelu@gmail.com>
2021-10-20 14:45:52 +0100

    Add iam additive resource (#2)

--- main.tf
+++ main.tf
@@ -14,6 +14,18 @@
  * limitations under the License.
  */
 
+locals {
+  iam_additive_list = flatten([
+    for role, members in var.iam_additive :
+    [for member in members : { role : role, member : member }]
+  ])
+
+  iam_additive_map = {
+    for v in local.iam_additive_list : "${v.role}/${v.member}" => v
+  }
+
+}
+
 resource "google_artifact_registry_repository" "registry" {
   provider      = google-beta
   project       = var.project_id
@@ -32,4 +44,14 @@ resource "google_artifact_registry_repository_iam_binding" "bindings" {
   repository = google_artifact_registry_repository.registry.name
   role       = each.key
   members    = each.value
+}
+
+resource "google_artifact_registry_repository_iam_member" "bindings_member" {
+  provider   = google-beta
+  for_each   = local.iam_additive_map
+  project    = var.project_id
+  location   = google_artifact_registry_repository.registry.location
+  repository = google_artifact_registry_repository.registry.name
+  role       = each.value.role
+  member     = each.value.member
 }
